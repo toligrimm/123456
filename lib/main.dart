494ca55fba3667fa304a0bd5577b1fdbfc701346
toligrimm <72781269+toligrimm@users.noreply.github.com>
2022-04-13 15:52:42 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:dismiss/todo_list.dart';
+
+
+
+void main() => runApp(Main());
+
+class Main extends StatelessWidget {
+  const Main({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterTodo',
+      home: Home(),
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+    );
+  }
+}
+
